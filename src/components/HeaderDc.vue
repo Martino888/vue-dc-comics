4148ce92e7063315da2dc82d481eb254d37919d1
Martino888 <maxmartino33@gmail.com>
2022-03-22 20:22:46 +0100

    header con problemi di border box

--- src/components/HeaderDc.vue
+++ src/components/HeaderDc.vue
@@ -1,20 +1,110 @@
 <template>
     <header>
-        <div class="color">ddddd</div>
-        <div class="colorw"></div>
-        <div class="color"></div>
+        <div class="header">.</div>
+        <div class="container">
+            <img class="logo" src="@/assets/img/dc-logo.png" alt="">
+            <ul>
+                <li v-for="(link, index) in links" :key="index"><a :href="link.href">{{link.text}}</a></li>
+            </ul>
+        </div>
     </header>
 
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-
+    name:'HeaderDc',
+    data () {
+        return {
+            links:[
+                {
+                href: '#',
+                text: 'CHARACTERS'
+            },
+            {
+                href: '#',
+                text: 'COMICS'
+            },
+            {
+                href: '#',
+                text: 'MOVIES'
+            },
+            {
+                href: '#',
+                text: 'TV'
+            },
+            {
+                href: '#',
+                text: 'GAMES'
+            },
+            {
+                href: '#',
+                text: 'COLLECTIBLES'
+            },
+            {
+                href: '#',
+                text: 'VIDEOS'
+            },
+            {
+                href: '#',
+                text: 'FANS'
+            },
+            {
+                href: '#',
+                text: 'NEWS'
+            },
+            {
+                href: '#',
+                text: 'SHOPS'
+            }
+            ]
+        }
+    }
 }
 </script>
 
-<style>
-.color{
-    background-color:black;
-}
+<style scoped lang="scss">
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    .header {
+        background-color: black;
+        height: 70px;
+    }
+
+    .container{
+        display:flex;
+        align-items:center;
+        justify-content: center;
+        background-color: white;
+    }
+
+    li{
+        display:inline-block;
+        padding: 5px;
+    }
+
+    a{
+        text-decoration:none;
+        color:black;
+    }
+
+    a:hover{
+        color:blue;
+    }
+
+    .logo{
+        width: 100px;
+        padding:5px;
+    }
+
+    .here{
+        width: 100%;
+        background-color: black;
+        color:white;
+    }
 </style>
---
+++ src/components/MainDc.vue
@@ -0,0 +1,87 @@
+<template>
+    <main>
+        <div class="container">
+            <h3>{{text}}</h3>
+        </div>
+        <div class="logosm">
+            <ul>
+                <li v-for="(link, index) in links" :key="index"><a :href="link.href">{{link.text}}</a></li>
+            </ul>
+        </div>
+    </main>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: "MainDc",
+    data(){
+        return {
+            text:"--> Content goes here<--",
+            links:[
+            {
+                href: '#',
+                text: 'CHARACTERS'
+            },
+            {
+                href: '#',
+                text: 'COMICS'
+            },
+            {
+                href: '#',
+                text: 'MOVIES'
+            },
+            {
+                href: '#',
+                text: 'TV'
+            },
+            {
+                href: '#',
+                text: 'GAMES'
+            },
+            ]
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+    color: white;
+    height: 70px;
+}
+li{
+    display:inline-block;
+    padding: 5px;
+}
+
+a{
+        text-decoration:none;
+        color: white;
+    }
+
+    a:hover{
+        color:blue;
+    }
+
+
+.logosm{
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+    justify-content: center;
+    background-color:blue;
+    color: white;
+    height: 100px;
+}
+</style>
