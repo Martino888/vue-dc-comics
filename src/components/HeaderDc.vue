a9aa9e3ec6b8f7e7da7841e60b2a2ea9610bf076
Martino888 <maxmartino33@gmail.com>
2022-03-22 18:28:13 +0100

    base2

---
+++ src/components/HeaderDc.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="color">ddddd</div>
+        <div class="colorw"></div>
+        <div class="color"></div>
+    </header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.color{
+    background-color:black;
+}
+</style>
