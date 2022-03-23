80de4dd1aabd4fb0c7285a53a2cc95499aeca0b8
Martino888 <maxmartino33@gmail.com>
2022-03-23 09:46:09 +0100

    con errore di base sul body

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,30 @@
   <div id="app">
     <HeaderDc />
     <MainDc />
+    <FooterMain />
   </div>
 </template>
 
 <script>
 import HeaderDc from './components/HeaderDc.vue'
 import MainDc from './components/MainDc.vue'
+import FooterMain from './components/FooterMain.vue'
 export default {
   name: 'App',
   components: {
     HeaderDc,
-    MainDc
+    MainDc,
+    FooterMain
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 }
 </style>
